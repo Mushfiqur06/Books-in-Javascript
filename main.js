ed37479b5aceb08de109773ed2b2ae47496a34ab
Mushfiqur06 <mushfiqur06@gmail.com>
2019-07-28 21:55:17 +0600

    Create Book Constructor and UI

---
+++ main.js
@@ -0,0 +1,45 @@
+//Book Constructor
+function Book(title, author, isbn){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+//UI Function
+function UI(){};
+
+//Add Book list to list
+UI.prototype.addBookList = function(book){
+    const list = document.getElementById('book-list');
+    //create tr element
+    const row = document.createElement('tr');
+    //insert cols
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">X</a></td>
+    `;
+    list.appendChild(row);
+}
+
+UI.prototype.showAlert = function(message, className){
+    //Create Div
+    const div = document.createElement('div');
+    //Create Classes
+    div.className = `alert ${className}`;
+    //Add text
+    div.appendChild(document.createTextNode(message));
+    //get parent
+    const container = document.querySelector('.container');
+    //Get form
+    const form = document.querySelector('#book-form');
+    //Insert Alert
+    container.insertBefore(div, form);
+
+    //Timeout after 3 sec
+    setTimeout(function(){
+        document.querySelector('.alert').remove()
+    }, 3000)
+};
+
